b1cd290a6bf99e6ad65857637ed15f99ec097417
Dabin Jeong <allempti@gmail.com>
2024-04-30 02:54:55 +0900

    ✅ Vue 싱글 파일 컴포넌트

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,26 +1,41 @@
+<!-- 
+  싱글 파일 컴포넌트: 화면의 특정 영역에 대한 HTML, CSS, JS 코드를 한 파일(.vue)에서 관리하는 방법
+  https://joshua1988.github.io/vue-camp/vue/sfc.html
+ -->
+
+<!-- HTML -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader v-bind:appTitle="message" v-on:change="changeMessage"></AppHeader>
 </template>
 
+<!-- JavaScript -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import 라이브러리 from '라이브러리이름'
+  // import 파일이름 from './파일경로'
+  import AppHeader from './components/AppHeader.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      // '컴포넌트 이름': 컴포넌트 내용
+      // 'AppHeader': AppHeader
+      // AppHeader: AppHeader
+      AppHeader
+    },
+    // 인스턴스 옵션 속성
+    data() {
+      return {
+        message: '앱 헤더 컴포넌트'
+      }
+    },
+    methods: {
+      changeMessage() {
+        this.message = '변경됨'
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- CSS -->
+<style scoped>
+
 </style>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ appTitle }}</h1>
+    <!-- v-on:click은 @click으로 축약 가능 -->
+    <button @click="changeTitle">변경</button>
+</template>
+
+<script>
+    export default {
+        props: ['appTitle'],
+        methods: {
+            changeTitle() {
+                this.$emit('change')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
